7410880504251bca166bbe5e23343b9072a0fe3c
Konstantin Varlamov <konstantinvarlamov@Konstantins-MacBook-Air.local>
2026-06-03 20:08:01 +0300

    one file

--- functional/lesson1-1.py
+++ functional/lesson1-1.py
@@ -1,5 +1,6 @@
 from pymonad.tools import curry
 
+# part 1
 @curry(2)
 def concat(a: str, b: str) -> str:
     return a + b
@@ -8,3 +9,15 @@ greet = concat("Hello, ")
 
 print(greet("World"))
 print(greet("Petya"))
+
+# part 2
+@curry(4)
+def make_greeting(firstword: str, comma: str, name: str, end: str) -> str:
+    return f"{firstword}{comma} {name}{end}"
+
+first_step = make_greeting("Hello")(",")
+final = first_step("!") 
+
+
+result = final("Petya")
+print(result) # Hello, Petya!
